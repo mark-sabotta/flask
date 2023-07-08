8c8be607b77b4caadc284e6f7d2054a578986336
Mark Sabotta <mark.sabotta@gmail.com>
2023-07-08 09:15:52 -0600

    implementing greet and calc functions

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,36 @@
-# Put your app in here.
+from flask import Flask
+from operations import *
+
+
+app = flask(__name__)
+
+
+@app.route('/add')
+def adding():
+    """Returns the sum of 2 parameters"""
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    return str(add(a, b))
+
+
+@app.route('/subtract')
+def subtract():
+    """It subtracts the second parameter from the first"""
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    return str(sub(a, b))
+
+@app.route('/multiply')
+def multiply(a, b):
+    """It returns the product of the parameters"""
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    return str(mult(a, b))
+
+@app.route('/divide')
+def divide():
+    """It divides the first parameter by the second
+    """
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    return div(a, b)
---
+++ greet/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = flask(__name__)
+
+@app.route('/welcome')
+def welcome():
+    """Returns 'welcome'"""
+    return 'welcome'
+
+
+
+@app.route('/welcome/back')
+def welcome_back():
+    """"Returns 'welcome back'"""
+    return 'welcome back'
+
+
+@app.route('/welcome/home')
+def welcome_home():
+    """"Returns 'welcome home, Scott'"""
+    return 'welcome home, Scott'
