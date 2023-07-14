c22c5e5ab20a04e84ad89c5bcb47f76d6bd2f0ed
Mark Sabotta <mark.sabotta@gmail.com>
2023-07-14 08:21:44 -0600

    creating flask-madlibs

--- calc/app.py
+++ calc/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from operations import add, mult, sub, div
-
+print("hi")
 
 app = Flask(__name__)
 
@@ -14,24 +14,25 @@ def adding():
     return str(result)
 
 
-@app.route('/subtract')
+@app.route('/sub')
 def subtract():
     """It subtracts the second parameter from the first"""
     a = int(request.args.get("a"))
     b = int(request.args.get("b"))
     return str(sub(a, b))
 
-@app.route('/multiply')
+@app.route('/mult')
 def multiply(a, b):
     """It returns the product of the parameters"""
     a = int(request.args.get("a"))
     b = int(request.args.get("b"))
     return str(mult(a, b))
 
-@app.route('/divide')
+@app.route('/div')
 def divide():
     """It divides the first parameter by the second
     """
     a = int(request.args.get("a"))
     b = int(request.args.get("b"))
     return str(div(a, b))
+
