ab763ee10c6c9e6d26868d5829947d5d7e90e4ea
Mark Sabotta <mark.sabotta@gmail.com>
2023-07-16 10:25:25 -0600

    testing server functionality

--- flask-madlibs/app.py
+++ flask-madlibs/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request
-from flask_debugtoolbar import DebugToolbarExtension
+#from flask_debugtoolbar import DebugToolbarExtension
 from stories import stories
 
 app = Flask(__name__)
 app.config['SECRET KEY'] = "secret"
 
-debug = DebugToolbarExtension(app)
+#debug = DebugToolbarExtension(app)
 
 @app.route("/")
 def ask_story():
